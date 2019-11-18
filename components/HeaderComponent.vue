3da98b7f8c2b65951e21fc9944b4900e5de1c8c8
regis <regis@bird-office.com>
2019-11-18 21:20:47 +0100

    add circularStd font

---
+++ components/HeaderComponent.vue
@@ -0,0 +1,6 @@
+<template>
+  <header class="">
+    <div class="col">Logo</div>
+    <div class="col w-25"></div>
+  </header>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div>
-      <logo />
+      <HeaderComponent />
       <h1 class="title">
         bird-office-front-end-test
       </h1>
@@ -25,44 +25,20 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import HeaderComponent from '~/components/HeaderComponent.vue'
 
 export default {
   components: {
-    Logo
+    HeaderComponent
   }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+body,
+* {
+  font-family: CircularStd, -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif,
+    'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
 }
 </style>
